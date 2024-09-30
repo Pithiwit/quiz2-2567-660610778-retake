1026ecdc8ef63843be9e8d635f2c2173bbefc707
Pithiwit <wazadafa14@gmail.com>
2024-09-30 16:16:13 +0700

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,17 @@ export default function Home() {
         style={{ maxWidth: "700px" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm bg-white"
       >
-        {/* Post Owner Example*/}
-      <PostOwnner userImagePath="/profileImages/profile778.jpg" username="Pithiwit Netkhamyuang 660610778" likes={100}/>
+        {/* Post Owner Example */}
+        <PostOwnner 
+          userImagePath="/profileImages/profile778.jpg" 
+          username="Pithiwit Netkhamyuang 660610778" 
+          likes={100}
+        />
         {/* map-loop render Comment component here */}
         {/* Comment Example */}
-        {comments.map((user) => (<Comment key = {user.username} {...user}/>))}
+        {comments.map((user) => (
+          <Comment key={user.username} {...user} />
+        ))}
       </div>
     </div>
   );
